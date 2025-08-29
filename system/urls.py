aff3a01132cb9fee84e86f62ec9df7fc40f800e4
demoprojectyt-del <demoproject.yt@gmail.com>
2025-08-29 05:38:09 -0400

    demo commit

---
+++ system/urls.py
@@ -0,0 +1,23 @@
+"""SalesManagementSystem URL Configuration"""
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+from django.contrib.auth import views as auth_views
+from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    
+    path('admin/', admin.site.urls),
+    path('',include('core.urls')),
+    path('register/', user_views.register, name='register'),
+    path('dashboard/', user_views.dashboard, name='dashboard'),
+    path('dashboard/updateprofile', user_views.update_profile, name='update-profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout', auth_views.logout_then_login, name='logout')
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
